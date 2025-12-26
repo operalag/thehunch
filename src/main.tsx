f40c61bffb70297d86335bb2205285d4b26cc9e1
Toni Caradonna <tonicaradonna@mac.home>
2025-12-26 13:59:33 +0100

    fix: Update configuration for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import App from "./App.tsx";
 import "./index.css";
 
-const manifestUrl = "https://operalag.github.io/hunch-demo-frontend/tonconnect-manifest.json";
+const manifestUrl = "https://hunch-demo-frontend.vercel.app/tonconnect-manifest.json";
 
 createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
