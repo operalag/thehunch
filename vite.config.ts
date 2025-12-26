330835d83ce90879a26faed00a2ed7a00dab24f5
Toni Caradonna <tonicaradonna@mac.home>
2025-12-26 13:50:56 +0100

    chore: Use conditional base path for dev vs prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hunch-demo-frontend/",
+  base: mode === "development" ? "/" : "/hunch-demo-frontend/",
   server: {
     host: "::",
     port: 8080,
